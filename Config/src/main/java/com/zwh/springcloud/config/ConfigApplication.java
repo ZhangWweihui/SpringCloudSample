87582a7246869dc6de7a03e4cfd2cb490968e096
weihuizhang@huashenghaoche.com <weihuizhang@huashenghaoche.com>
2019-03-04 16:24:02 +0800

    添加配置中心

---
+++ Config/src/main/java/com/zwh/springcloud/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.zwh.springcloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author ZhangWeihui
+ * @date 2019/3/4 16:20
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}

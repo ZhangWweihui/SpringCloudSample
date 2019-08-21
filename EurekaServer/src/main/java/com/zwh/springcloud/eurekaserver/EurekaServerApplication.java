e1f107f43cc9c018e5ada3857c2f3239ed710ae4
weihuizhang@huashenghaoche.com <weihuizhang@huashenghaoche.com>
2019-08-21 17:55:31 +0800

    将SpringCloud的版本从D版升级为G版

--- EurekaServer/src/main/java/com/zwh/springcloud/eurekaserver/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/zwh/springcloud/eurekaserver/EurekaServerApplication.java
@@ -1,7 +1,8 @@
 package com.zwh.springcloud.eurekaserver;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
@@ -9,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 
 }
--- Feign/src/main/java/com/zwh/springcloud/feign/FeignApplication.java
+++ Feign/src/main/java/com/zwh/springcloud/feign/FeignApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@EnableHystrixDashboard
 @EnableCircuitBreaker
 public class FeignApplication {
 
